3425c4ba207144fa362d20c5f169fb9b932ce678
Greg Dodd <31262364+gregdodd@users.noreply.github.com>
2023-01-26 16:39:59 -0500

    Update init.lua

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -16,9 +16,30 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
+     use 'wbthomason/packer.nvim'
+     -- My plugins here
+     -- use 'foo1/bar1.nvim'
+     -- use 'foo2/bar2.nvim'
+      use {
+         'nvim-telescope/telescope.nvim', tag = '0.1.1',
+         -- or                            , branch = '0.1.x',
+         requires = { {'nvim-lua/plenary.nvim'}}
+     }
 
+     use({
+         'Mofiqul/dracula.nvim',
+         as = 'dracula',
+         config = function()
+             require("dracula").setup()
+             vim.cmd('colorscheme dracula')
+          end
+     })
+
+     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+     use('nvim-treesitter/playground')
+     use('ThePrimeagen/harpoon')
+     use('mbbill/undotree')
+     use('tpope/vim-fugitive')
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
